fcdb621f4cb4200dfde424caffdbadc7a6164ef8
svigeland <s.vigeland@gmail.com>
2018-05-19 11:55:02 -0500

    modifications to runscript.sh

--- codes/runscript.sh
+++ codes/runscript.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 # script for generating and analyzing many datasets
-# to modify, change the numbers 30 and 33 to correspond to the numbers 
+# to modify, change the numbers 1 and 9 to correspond to the numbers 
 # of the first and last set you want to generate
 
-for i in $(seq 0 1 9)
+for i in $(seq 1 1 9)
 do
-	python optstat_v2.py --datasetname dataset_nano_$i --computeMonopole --computeDipole --computeSkyScrambles
+    python optstat_v3.py --Agw 5e-15 --datasetname dataset_nano_$i --nreal 10000 --computeMonopole --computeDipole
 done
